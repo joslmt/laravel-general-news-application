2887cdfbb6c9369dd69cfb1f88db8ec917f6a439
unknown <joseluis95123@gmail.com>
2021-08-19 21:44:00 +0200

    Add contextual binding

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,18 +2,27 @@
 
 namespace App\Providers;
 
+use App\Contracts\NewsInterface;
+use App\Http\Controllers\NewsAPIController;
+use App\Models\NewsAPI;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
-     *
+     * 
+     * Using Contextual binding for future implementations.
+     * 
      * @return void
+     * 
+     * @see https://laravel.com/docs/8.x/container#contextual-binding
      */
     public function register()
     {
-        //
+        $this->app->when(NewsAPIController::class)
+            ->needs(NewsInterface::class)
+            ->give(NewsAPI::class);
     }
 
     /**
