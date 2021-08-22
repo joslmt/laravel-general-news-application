a90cbfc85dab03fa6b2ca6edc7b2435341ab94ca
unknown <joseluis95123@gmail.com>
2021-08-22 16:54:23 +0200

    Change latest news interface, more minimalistic

--- resources/views/components/latest.blade.php
+++ resources/views/components/latest.blade.php
@@ -1,34 +1,27 @@
 <x-app-layout>
     <section class="text-gray-600 body-font overflow-hidden">
         <div class="text-4xl max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-            Breaking News
+            Total Breaking News {{ count($latestNews['articles']) }}
         </div>
         <div class="container px-5 py-24 mx-auto">
 
             @foreach ($latestNews['articles'] as $latestNew)
-
             <div class="-my-8 divide-y-2 divide-gray-100">
                 <div class="py-8 flex flex-wrap md:flex-nowrap">
                     <div class="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                         <span class="font-semibold title-font text-gray-700">Source: {{ $latestNew['source']['name'] }}</span>
+                        <a href="{{ $latestNew['urlToImage'] }}">
+                            <img src="{{ $latestNew['urlToImage'] }}" class="w-20 my-3 md:w-32 lg:w-56 border rounded" alt="latestNews">
+                        </a>
                         <span class="mt-1 text-gray-500 text-sm">
-                            {{ $latestNew['publishedAt'] }}
+                            {{ substr($latestNew['publishedAt'], 0, 10) }}
                         </span>
-                        <img src="{{ $latestNew['urlToImage'] }}" class="w-20 md:w-32 lg:w-48" alt="latestNews">
                     </div>
                     <div class="md:flex-grow">
                         <div class="flex self-align gap-3">
                             <h2 class="text-2xl font-medium text-gray-900 title-font mb-2">
                                 {{ $latestNew['title'] }}
                             </h2>
-                            <form action="#" method="post">
-                                @csrf
-                                <button type="submit"">
-                                    <svg xmlns=" http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 5a2 2 0 012-2h10a2 2 0 012 2v16l-7-3.5L5 21V5z" />
-                                    </svg>
-                                </button>
-                            </form>
                         </div>
                         <p class="leading-relaxed">
                             {{ $latestNew['description'] }}
@@ -43,6 +36,7 @@
                 </div>
             </div>
             @endforeach
+
             <div class="flex justify-end">
                 <button onclick="window.history.back()" type="submit" class="mt-2 lg:w-1/4 inline-flex text-white bg-gray-500 border-0 py-2.5 px-6 focus:outline-none hover:bg-gray-600 rounded text-lg">Back</button>
             </div>
